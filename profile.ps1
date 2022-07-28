9be2849bc19328cd6931c9a3022a8b1b831fad4e
StefanGreve <greve.stefan@outlook.jp>
2022-07-28 23:40:16 +0200

    Refactor code and implement Start-Timer
    * implement a simplified version of the old Start-Timer cmdlet without
      the keypress parameter (since that paraemeter depended on a
      platform-specific assembly from Windows)
    * simplify the progress bar invocation in Get-XKCD

--- profile.ps1
+++ profile.ps1
@@ -456,7 +456,8 @@ function Get-XKCD {
             $XKCD = [XKCD]::new($Id, $Path, $Client)
 
             if (-not $NoDownload.IsPresent -and $PSCmdlet.ShouldProcess($XKCD.Img, "Download $($XKCD.Path)")) {
-                Write-Progress -Activity "Download XKCD $Id" -PercentComplete ($i * 100 / $Ids.Count) -Status "$(([Math]::Round(($i / $Ids.Count * 100), 0)))%"
+                [int] $PercentComplete = [Math]::Round($i / $Ids.Count * 100, 0)
+                Write-Progress -Activity "Download XKCD $Id" -Status "$PercentComplete%" -PercentComplete $PercentComplete
                 $XKCD.Download($Force.IsPresent)
             }
 
@@ -666,6 +667,48 @@ function Measure-ScriptBlock {
     }
     end {
         Write-Output $($Measurements | Measure-Object -Property Ticks -AllStats)
+    }
+}
+
+function Start-Timer {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ParameterSetName = "Seconds")]
+        [int] $Seconds,
+
+        [Parameter(Mandatory, ParameterSetName = "Minutes")]
+        [int] $Minutes,
+
+        [Parameter(Mandatory, ParameterSetName = "Hours")]
+        [int] $Hours
+    )
+
+    begin {
+        $CountDown = switch ($PSCmdlet.ParameterSetName) {
+            "Seconds" {
+                $Seconds
+            }
+            "Minutes" {
+                $Minutes * 60
+            }
+            "Hours" {
+                $Hours * 3600
+            }
+        }
+
+        $StartDate = [DateTime]::Now
+        $StopWatch = [Stopwatch]::StartNew()
+        [int] $t = 0
+    }
+    process {
+        while ($t -le $CountDown) {
+            [int] $PercentComplete = [Math]::Round($t * 100 / $CountDown, 0)
+            Write-Progress -Activity "Timer" -Status "$PercentComplete%" -PercentComplete $PercentComplete -SecondsRemaining ($CountDown - $t)
+            $t = $StopWatch.Elapsed.TotalSeconds
+        }
+    }
+    end {
+        Write-Verbose "Timer finished after $CountDown seconds (start date: $StartDate)"
     }
 }
 
