1d532b71d8ac7d0c2f8a949302cbd5591e887bd2
StefanGreve <greve.stefan@outlook.jp>
2023-06-18 20:02:11 +0200

    Bump version number

--- profile.ps1
+++ profile.ps1
@@ -16,7 +16,7 @@ using namespace Microsoft.PowerShell
 $global:ProfileVersion = [PSCustomObject]@{
     Major = 1
     Minor = 5
-    Patch = 0
+    Patch = 1
 }
 
 $global:OperatingSystem = if ([OperatingSystem]::IsWindows()) {
