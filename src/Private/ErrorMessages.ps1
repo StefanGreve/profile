878a0813e6e071160a5163b0a32ac0246158828e
StefanGreve <greve.stefan@outlook.jp>
2024-11-25 21:33:37 +0100

    Supress error messages

--- src/Private/ErrorMessages.ps1
+++
@@ -1,1 +0,0 @@
-$OperatingSystemNotSupportedError = "This Cmdlet is not supported on your current operating system."
---
+++ src/Private/Variables.ps1
@@ -0,0 +1,6 @@
+using namespace System.Diagnostics.CodeAnalysis
+
+[SuppressMessage("PSUseDeclaredVarsMoreThanAssignments", "")]
+param()
+
+$OperatingSystemNotSupportedError = "This Cmdlet is not supported on your current operating system."
