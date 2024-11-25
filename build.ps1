034e851f47b7fcfa697a398abefb808a3fad8e5c
StefanGreve <greve.stefan@outlook.jp>
2024-11-25 20:45:54 +0100

    Add build script

---
+++ build.ps1
@@ -0,0 +1,54 @@
+param(
+    [string] $ModuleName = "Toolbox"
+)
+
+begin {
+    Push-Location "src"
+
+    $Steps = 3
+    $ManifestPath = "${ModuleName}.psd1"
+}
+process {
+    #region Step 1 - Test Module Manifest
+
+    Write-Host "[1/${Steps}] " -ForegroundColor DarkGray -NoNewline
+    Write-Host "Test Module Manifest"
+    Test-ModuleManifest -Path $ManifestPath
+    Write-Host
+
+    #endregion
+
+    #region Step 2 - Update Manifest
+
+    Write-Host "[2/${Steps}] " -ForegroundColor DarkGray -NoNewline
+    Write-Host "Update Manifest"
+
+    $FunctionsToExport = Get-ChildItem -Path "./Public" -Filter "*.ps1"
+        | Select-Object -ExpandProperty BaseName
+
+    $FileList = Get-ChildItem -Recurse -Path "."
+        | Where-Object { ! $_.PSIsContainer }
+        | Select-Object -ExpandProperty FullName
+        | Resolve-Path -Relative
+
+    $ManifestParameter = @{
+        Path = $ManifestPath
+        FunctionsToExport = @($FunctionsToExport)
+        FileList = @($FileList)
+    }
+
+    Update-ModuleManifest @ManifestParameter
+
+    #endregion
+
+    #region Step 3 - Import Module
+
+    Write-Host "[3/${Steps}] " -ForegroundColor DarkGray -NoNewline
+    Write-Host "Import Module"
+    Import-Module -Name "./${ManifestPath}" -Force -Verbose
+
+    #endregion
+}
+clean {
+    Pop-Location
+}
