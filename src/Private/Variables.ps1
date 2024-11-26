4b1f60ff31c9b77d723d4dfd371f7992dc0c30f1
StefanGreve <greve.stefan@outlook.jp>
2024-11-26 21:17:20 +0100

    Directly link to issues tab in help link

--- src/Private/Variables.ps1
+++ src/Private/Variables.ps1
@@ -5,7 +5,7 @@ param()
 
 #region Error Messages
 
-$Disclaimer = "To report an issue, use the following link: https://github.com/stefangreve/profile"
+$Disclaimer = "To report an issue, use the following link: https://github.com/stefangreve/profile/issues"
 
 $OperatingSystemNotSupportedError = "This Cmdlet is not supported on your current operating system. ${Disclaimer}"
 $ParameterOverloadNotImplementedError = "This method invocation is not implemented yet. ${Disclaimer}"
