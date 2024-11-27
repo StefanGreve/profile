55ef1bb61c36aa06a5aff9cb24f398c8f50313cc
StefanGreve <greve.stefan@outlook.jp>
2024-11-27 20:24:21 +0100

    Fix Get-RandomPassword Cmdlet

--- src/Public/Get-RandomPassword.ps1
+++ src/Public/Get-RandomPassword.ps1
@@ -8,17 +8,14 @@ function Get-RandomPassword {
         [int] $Length = 64
     )
 
-    begin {
+    process {
         # Base64 encoding encodes every 3 bytes of input data into 4 characters
         # of output data. The required length of the password can be computed by
         $Size = [Math]::Floor($Length * 3 / 4)
         $Buffer = [byte[]]::new($Size)
-    }
-    process {
+
         [RandomNumberGenerator]::Fill($Buffer)
         $Password = [Convert]::ToBase64String($Buffer)
-    }
-    clean {
         Write-Output $Password
     }
 }
