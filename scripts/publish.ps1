120b082c79a80984ea981f74114c9aa79e2c99fb
StefanGreve <greve.stefan@outlook.jp>
2024-11-29 12:25:03 +0100

    Create deployment script

---
+++ scripts/publish.ps1
@@ -0,0 +1,39 @@
+using namespace System.IO
+
+[CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
+param(
+    [string] $ModuleName = "Toolbox",
+
+    [Parameter(Mandatory)]
+    [string] $ApiKey,
+
+    [Parameter(Mandatory)]
+    [string] $Version
+)
+
+begin {
+    $ManifestPath = "${ModuleName}.psd1"
+
+    $ScriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Path
+    $ProjectRoot = $(Get-Item $([Path]::Combine($ScriptPath, ".."))).FullName
+    Push-Location $ProjectRoot
+}
+process {
+    # 1 - Build
+    & "./scripts/build.ps1"
+
+    # 2 - Test
+    & "./scripts/test.ps1"
+
+    # 3 - Deploy
+    if ($PSCmdlet.ShouldProcess($ManifestPath, "Publish `"${ModuleName}`" (Version ${Version}) to PSGallery")) {
+        Publish-Module -Name "./src/${ManifestPath}" `
+            -NuGetApiKey $ApiKey `
+            -RequiredVersion $Version
+    }
+}
+clean {
+    Pop-Location
+}
+
+
