c40dac83967f2c2290f37d33d27120d9276dac2f
StefanGreve <greve.stefan@outlook.jp>
2024-12-29 14:44:00 +0100

    Add doc strings to Get-Definition

--- src/Public/Get-Definition.ps1
+++ src/Public/Get-Definition.ps1
@@ -1,4 +1,28 @@
 function Get-Definition {
+    <#
+        .SYNOPSIS
+        Prints the definition of a specified PowerShell function or Cmdlet.
+
+        .DESCRIPTION
+        Prints the definition of a specified PowerShell function or Cmdlet.
+        If the command is a PowerShell script or function, the source code of the script or function is returned.
+        Otherwise, for commands from binary modules, the syntax of the command is displayed.
+
+        .PARAMETER Command
+        The name of the PowerShell command.
+
+        .INPUTS
+        None. You can't pipe objects to Get-Definition.
+
+        .EXAMPLE
+        PS> Get-Definition Get-Battery
+
+        .OUTPUTS
+        The definition of the specified command is returned as a string.
+
+        .NOTES
+        If the bat syntax highlighter is installed and accessible, it formats the output.
+    #>
     [OutputType([string])]
     param(
         [string] $Command
