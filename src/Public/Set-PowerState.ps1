fd3dc004123f3bd9f3357589df0e907d3ff884ee
StefanGreve <greve.stefan@outlook.jp>
2025-01-04 20:47:49 +0100

    Add doc strings to Set-PowerState

--- src/Public/Set-PowerState.ps1
+++ src/Public/Set-PowerState.ps1
@@ -3,6 +3,42 @@ using namespace System.Collections.ObjectModel
 using namespace System.Management.Automation
 
 function Set-PowerState {
+    <#
+        .SYNOPSIS
+        Sets the power state of the system to either Hibernate or Suspend.
+
+        .DESCRIPTION
+        This function adjusts the power state of the system based on the specified
+        power mode and allows for optional parameters to handle wake events and
+        force the operation.
+
+        .PARAMETER PowerState
+        Specifies the desired power state.
+
+        .PARAMETER DisableWake
+        Disables all wake events to prevent the system from waking
+        automatically during sleep or hibernation. This parameter is only available
+        on Windows operating systems.
+
+        .PARAMETER Force
+         Forces the power state change, bypassing confirmations or warnings.
+
+        .INPUTS
+        None. You can't pipe objects to Set-PowerState.
+
+        .EXAMPLE
+        PS> Set-PowerState
+
+        Sets the power state to suspend.
+
+        .EXAMPLE
+        PS> Set-PowerState -PowerState Hibernate -DisableWake -Force
+
+        Puts the system into suspend mode and disables all wake events.
+
+        .OUTPUTS
+        None. This function does not produce any output.
+    #>
     [OutputType([void])]
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
     param(
