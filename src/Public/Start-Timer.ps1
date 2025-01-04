7e6204498aab6858659a352ce7e63e97306bc487
StefanGreve <greve.stefan@outlook.jp>
2025-01-04 20:41:54 +0100

    Add doc strings to Start-Timer

--- src/Public/Start-Timer.ps1
+++ src/Public/Start-Timer.ps1
@@ -2,6 +2,41 @@ using namespace System
 using namespace System.Diagnostics
 
 function Start-Timer {
+    <#
+        .SYNOPSIS
+        Starts a countdown timer and provides real-time progress updates.
+
+        .DESCRIPTION
+        The Start-Timer function starts a countdown timer for a specified duration.
+        The duration can be provided in seconds, minutes, or hours, depending on the
+        parameter set used. The function displays a progress bar indicating the
+        percentage completed and the estimated time remaining.
+
+        .PARAMETER Seconds
+        Specifies the duration of the timer in seconds.
+
+        .PARAMETER Minutes
+        Specifies the duration of the timer in minutes.
+
+        .PARAMETER Hours
+        Specifies the duration of the timer in hours.
+
+        .INPUTS
+        None. You can't pipe objects to Start-Timer.
+
+        .EXAMPLE
+        PS> Start-Timer -Seconds 30
+
+        Starts a 30-second countdown timer and displays the progress bar.
+
+        .EXAMPLE
+        PS> Start-Timer -Hours 1
+
+        Starts a 1-hour countdown timer and displays the progress bar.
+
+        .OUTPUTS
+        None. This function does not produce any output.
+    #>
     [OutputType([void])]
     [CmdletBinding()]
     param(
