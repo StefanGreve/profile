82e4686322a5980fe088d30e9b2d6051517b8af5
StefanGreve <greve.stefan@outlook.jp>
2025-01-04 21:25:53 +0100

    Add doc strings to Invoke-XKCD

--- src/Public/Invoke-XKCD.ps1
+++ src/Public/Invoke-XKCD.ps1
@@ -3,6 +3,83 @@ using namespace System.Net.Http
 using namespace System.Net.Http.Headers
 
 function Invoke-XKCD {
+    <#
+        .SYNOPSIS
+        Retrieves XKCD comics based on the specified parameters.
+
+        .DESCRIPTION
+        XKCD is a serial web comic created in 2005 by American author Randall Munroe.
+        The comic's tagline describes it as "a web comic of romance, sarcasm, math,
+        and language". Munroe states on the comic's website that the name of the
+        comic is not an initialism but "just a word with no phonetic pronunciation".
+
+        .PARAMETER All
+        Retrieves all available XKCD comics.
+
+        .PARAMETER Number
+         Retrieves specific XKCD comic based on its identifier (ID).
+
+        .PARAMETER Random
+        Retrieves a random XKCD comic.
+
+        .PARAMETER From
+         Specifies the starting number of a range of XKCD comics to retrieve.
+         Must be used with the -To parameter.
+
+        .PARAMETER To
+        Specifies the ending number of a range of XKCD comics to retrieve.
+        Must be greater than the value of -From.
+
+        .PARAMETER Last
+        Retrieves the last N comics, where N is specified by this parameter.
+
+        .PARAMETER Path
+        Specifies the directory where downloaded XKCD comics will be saved.
+        The default path is the current working directory.
+
+        .PARAMETER Download
+         Indicates that the retrieved XKCD comics should be downloaded to the
+         specified -Path.
+
+        .PARAMETER Force
+        Forces overwriting of existing files during download operations, if they
+        already exist in the specified path.
+
+        .INPUTS
+        None. You can't pipe objects to Invoke-XKCD.
+
+        .EXAMPLE
+        PS> Invoke-XKCD -Number 42
+
+        Retrieves information about the XKCD comic #42.
+
+        .EXAMPLE
+        PS> Invoke-XKCD -Random
+
+        Retrieves a random XKCD comic.
+
+        .EXAMPLE
+        PS> Invoke-XKCD -From 10 -To 20
+
+        Retrieves XKCD comics numbered 10 through 20.
+
+        .EXAMPLE
+        PS> Invoke-XKCD -Last 1 -Download -Path "C:\Comics"
+
+        Downloads the latest XKCD comic to the "C:\Comics" directory.
+
+        .EXAMPLE
+        PS> Invoke-XKCD -All -Download
+
+        Downloads all XKCD comics to the current working directory.
+
+        .OUTPUTS
+        XKCD. The function returns a XKCD object with properties describing the comic.
+
+        .LINK
+        https://xkcd.com/
+        https://en.wikipedia.org/wiki/Xkcd
+    #>
     [Alias("xkcd")]
     [OutputType([XKCD])]
     [CmdletBinding(DefaultParameterSetName = "Last", SupportsShouldProcess, ConfirmImpact = "Low")]
@@ -110,7 +187,7 @@ function Invoke-XKCD {
                     $Response.year,
                     $Response.month,
                     $Response.day,
-                    $Response.link
+                    "https://xkcd.com/$Id/"
                 ))
             } catch {
                 Write-Error "A comic with ID=${Id} does not exist." -Category InvalidArgument -ErrorAction Stop
