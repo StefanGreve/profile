eb7a42877edac5eb803c744ed08ed0af80f1963b
StefanGreve <greve.stefan@outlook.jp>
2025-01-05 18:34:19 +0100

    Add doc strings to Get-EnvironmentVariable

--- src/Public/Get-EnvironmentVariable.ps1
+++ src/Public/Get-EnvironmentVariable.ps1
@@ -1,6 +1,41 @@
 using namespace System
 
 function Get-EnvironmentVariable {
+    <#
+        .SYNOPSIS
+        Reads an environment variable from the specified scope.
+
+        .DESCRIPTION
+        Reads an environment variable from the specified scope. If no key is provided,
+        it returns all environment variables available in the scope.
+        The default scope is 'Process'.
+
+        .PARAMETER Key
+        The name of the environment variable to read.
+
+        .PARAMETER Scope
+        Specifies the scope of the environment variable to read.
+        The default is Process.
+
+        .INPUTS
+        None. You can't pipe objects to Get-EnvironmentVariable.
+
+        .EXAMPLE
+        PS> Get-EnvironmentVariable -Scope Machine
+
+        Returns all values from the PATH environment variable defined in Machine scope.
+
+        .EXAMPLE
+        PS> Get-EnvironmentVariable -Key PROFILE_ENABLE_BRANCH_USERNAME -Scope User
+
+        Returns all values from the PROFILE_ENABLE_BRANCH_USERNAME environment variable defined in User scope.
+
+        .OUTPUTS
+        System.String[]. A collection of strings representing the values of the retrieved environment variable(s).
+
+        .LINK
+        https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_environment_variables
+    #>
     [OutputType([string[]])]
     [CmdletBinding()]
     param(
