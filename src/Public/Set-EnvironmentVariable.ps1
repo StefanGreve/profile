9d62fb160b92898c328573ec30798410b6d52e7c
StefanGreve <greve.stefan@outlook.jp>
2025-01-05 18:47:22 +0100

    Add doc strings to Set-EnvironmentVariable

--- src/Public/Set-EnvironmentVariable.ps1
+++ src/Public/Set-EnvironmentVariable.ps1
@@ -1,6 +1,58 @@
 using namespace System
 
 function Set-EnvironmentVariable {
+    <#
+        .SYNOPSIS
+        Sets an environment variable in the specified scope.
+
+        .DESCRIPTION
+        Defines or modifies an environment variable in the specified scope.
+        The scope determines where the variable will be available. If the scope
+        is User or Machine, changes may require restarting the terminal or system
+        to take effect.
+
+        .PARAMETER Key
+        The name of the environment variable to set.
+
+        .PARAMETER Value
+         The value to assign to the environment variable.
+
+        .PARAMETER Scope
+        Specifies the scope of the environment variable to set.
+        The default is Process. The terminal session requires a restart if the
+        scope is not set to Process for the changes to take effect
+
+        .PARAMETER Override
+        If specified, the function overwrites the existing value of the environment
+        variable if it already exists.
+
+        .PARAMETER Force
+        Enable this option to add a value to an existing key multiple times.
+
+        .INPUTS
+        None. You can't pipe objects to Set-EnvironmentVariable.
+
+        .EXAMPLE
+        PS> Set-EnvironmentVariable -Key PROFILE_ENABLE_BRANCH_USERNAME -Value 1
+
+        Sets the value of the PROFILE_ENABLE_BRANCH_USERNAME environment variable to 1 in Process scope.
+
+        .EXAMPLE
+        PS> Set-EnvironmentVariable -Key API_KEY -Value "REDACTED" -Scope Process -Override
+
+        Sets the value of the API_KEY environment variable to "REDACTED" in the User scope, overwriting any existing value.
+
+        .EXAMPLE
+        PS> Set-EnvironmentVariable -Key PATH -Value "C:\NewPath" -Scope Machine -Force
+
+        Adds "C:\NewPath" to the PATH environment variable in the Machine scope, even if the PATH variable already exists.
+
+        .OUTPUTS
+        None. This function does not produce any output.
+
+        .LINK
+        https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_environment_variables
+    #>
     [OutputType([void])]
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Low")]
     param(
