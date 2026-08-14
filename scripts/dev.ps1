8864afff654a436804f5e89cd461c2053dec584a
StefanGreve <stefan.ohlsen.greve@gmail.com>
2026-08-14 22:21:25 +0200

    Add convenience script for local development

---
+++ scripts/dev.ps1
@@ -0,0 +1,22 @@
+using namespace System.IO
+
+begin {
+    $ModuleName = "PowerTools"
+    $ManifestPath = "${ModuleName}.psd1"
+    $ProjectRoot = Split-Path -Path $PSScriptRoot -Parent
+
+    Push-Location $ProjectRoot
+}
+process {
+    # 1 - Unload the currently installed module
+    Remove-Module -Name $ModuleName -Force -ErrorAction SilentlyContinue
+
+    # 2 - Build a local development version
+    & "./scripts/build.ps1" -ModuleName $ModuleName -Version 0.0.0
+
+    # 3 - Import the freshly built module
+    Import-Module -Name "./src/${ManifestPath}" -Force -ErrorAction Stop
+}
+clean {
+    Pop-Location
+}
